e3fb6ad7afc739c0fb3fdf7402f6d0e19c4d07ee
zoe2276 <zoe22761@gmail.com>
2025-12-27 23:47:11 -0600

    adjustment of settings to be more composable, using localstorage and cssom

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -2,12 +2,43 @@ import * as React from "react"
 import { FAIcon } from "./icons"
 import "./styles/Settings.css"
 
-export const Settings = () => {
-    const [backgroundColor, setBackgroundColor] = React.useState("#ffb6c1dd")
+interface DefaultSettings {
+    "accent-color": string,
+    "background-color": string
+}
 
-    const updateBackgroundColor = (color: string) => {
-        setBackgroundColor(color)
-        document.querySelector("body").style = `background-color: ${color}`
+export const Settings = () => {
+    const defaults: DefaultSettings = {
+        "accent-color": "#524552",
+        "background-color": "#df96a1"
+    }
+
+    const getStylesheet = (variable: string) => {
+        type dVar = keyof DefaultSettings
+        return document.documentElement.style.getPropertyValue(`--${variable}`) || defaults[variable as dVar] || ""
+    }
+
+    const updateStylesheet = (variable: string, value: string) => {
+        document.documentElement.style.setProperty(`--${variable}`, value)
+
+        // use k,v pairs with json to encode into localstorage instead of raw css
+        try {
+            const rs = window.localStorage.getItem("userSettings")
+            const settings = rs ? JSON.parse(rs) as Record<string, string> : {}
+            settings[variable] = value
+            window.localStorage.setItem("userSettings", JSON.stringify(settings))
+        } catch { /* yum yum swallow errors */ }
+
+        /* old way
+        const ss = document.querySelector("style#userSettings") as HTMLStyleElement
+        if (ss?.textContent.includes(`--${variable}:`)) {
+            ss.textContent = ss.textContent.replace(new RegExp(`--${variable}: [^;]+;`), `--${variable}: ${value};`)
+        } else {
+            ss.textContent = ss.textContent.replace("}", `--${variable}: ${value};\n}`)
+        }
+
+        window.localStorage.setItem("userSettings", ss.textContent)
+        */
     }
 
     const showSettingsMenu = () => {
@@ -34,6 +65,26 @@ export const Settings = () => {
         })
     }
 
+    React.useEffect(() => {
+        try {
+            const rs = window.localStorage.getItem("userSettings")
+            if (!rs) return
+            const settings = JSON.parse(rs) as Record<string, string>
+            for (const [k, v] of Object.entries(settings)) {
+                document.documentElement.style.setProperty(`--${k}`, v)
+            }
+        } catch { /* yum yum */ }
+        /* old way
+        let styleSheet = document.querySelector("style#userSettings") as HTMLStyleElement
+        if (!styleSheet) {
+            styleSheet = document.createElement("style")
+            styleSheet.id = "userSettings"
+            styleSheet.appendChild(document.createTextNode(window.localStorage.getItem("userSettings") || ":root {\n\n}"))
+            document.querySelector("head")?.appendChild(styleSheet)
+        }
+        */
+    }, [])
+
     return (
         <>
             <button className="settingsButton" onClick={() => showSettingsMenu()}>
@@ -44,8 +95,20 @@ export const Settings = () => {
                 <button id="settingsMenu-close" onClick = {() => hideSettingsMenu()}>
                     <FAIcon iconName="x" />
                 </button>
-                Background Color
-                <input type="color" onChange={e => updateBackgroundColor(e.target.value)} />
+                <ul className="settingsMenu-items">
+                    <li>
+                        <div>
+                            Background Color
+                            <input type="color" onChange={e => updateStylesheet("background-color", e.target.value)} defaultValue={getStylesheet("background-color")} />
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                            Accent Color
+                            <input type="color" onChange={e => updateStylesheet("accent-color", e.target.value)} defaultValue={getStylesheet("accent-color")} />
+                        </div>
+                    </li>
+                </ul>
             </div>
         </>
     )

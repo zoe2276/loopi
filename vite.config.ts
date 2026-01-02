4aeec2ca1eb15a49044c4b001ec75404e94891f0
zoe2276 <zoe22761@gmail.com>
2026-01-01 22:39:10 -0600

    add base route to config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/projects/loopi",
   plugins: [react()],
   server: {
     host: "0.0.0.0"
